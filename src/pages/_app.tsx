887cdea420e6b70f59e14eeeed8fe98d71d98c4d
Ryan Chen <ryanchen@aetheras.io>
2023-09-26 18:59:45 +0800

    feat: setup tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,29 @@
+import '@/constants/globals.css'
+
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>{'Website Title'}</title>
+        <meta
+          name={'viewport'}
+          content={'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no'}
+        />
+
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
+              :root {
+              }
+            `
+          }}
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
