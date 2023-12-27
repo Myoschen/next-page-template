88ed94b4eee4fb7f778033c9dc97485bc321fa25
Ryan Chen <ryanchen@aetheras.io>
2023-12-27 12:05:02 +0800

    chore: update next config

--- next.config.js
+++ next.config.js
@@ -1,34 +1,29 @@
-// Show Current Git Branch and Commit Hash
-const getGitInfo = () => {
+/**
+ * @description Show current git branch and commit hash.
+ */
+function getGitInfo() {
   try {
     const execSync = require('child_process').execSync
     const gitBranch = execSync('git rev-parse --abbrev-ref HEAD')?.toString()?.trim() ?? ''
     const gitCommitHash = execSync('git rev-parse HEAD')?.toString()?.trim() ?? ''
     const buildId = `${gitBranch}_${gitCommitHash.substring(0, 7)}`
-
     return { gitBranch, gitCommitHash, buildId }
   } catch (error) {
     console.log('get git info failed.')
-
     return undefined
   }
 }
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  eslint: { ignoreDuringBuilds: true },
+  images: { unoptimized: true }
+}
+
 module.exports = () => {
   const gitInfo = getGitInfo()
-
-  return {
-    output: 'export',
-    reactStrictMode: true,
-    trailingSlash: true,
-    eslint: {
-      ignoreDuringBuilds: true
-    },
-    images: {
-      unoptimized: true
-    },
-    env: {
-      ...gitInfo
-    }
-  }
+  return { ...nextConfig, env: { ...gitInfo } }
 }
